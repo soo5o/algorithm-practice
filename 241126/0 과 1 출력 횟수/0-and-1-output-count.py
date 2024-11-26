a192084c4ba15f955f2b879f40afea0808019726
Soo <90204622+soo5o@users.noreply.github.com>
2024-11-26 18:11:56 +0900

    Title: 0 과 1 출력 횟수, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 241126/0 과 1 출력 횟수/0-and-1-output-count.py
+++ 241126/0 과 1 출력 횟수/0-and-1-output-count.py
@@ -2,7 +2,6 @@ w = int(input())
 dp = []
 dp.append([1, 0])
 dp.append([0, 1])
-dp.append([1, 1])
-for i in range(2, w):
+for i in range(2, w+1):
     dp.append([dp[i-1][0]+dp[i-2][0], dp[i-1][1]+dp[i-2][1]])
 print(dp[w][0], dp[w][1])
